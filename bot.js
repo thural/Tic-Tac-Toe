8b8795f338e98b5ed2327c265b8db62a30c52cfe
Thural Musaibov <tural.musaibov@gmail.com>
2022-05-20 22:31:09 +0400

    refactor more code into objects

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const botFactory = (mode) => {
+    let turn = true;
 
     const concatArrays = (arraysInArray) => {
         let concat = [];
@@ -20,9 +21,9 @@ const botFactory = (mode) => {
         return unique
     };
 
-    const getLinesWith = (numMark, mark) => {
+    const getLines = (markCount, mark) => {
 
-        let numEmpty = 3 - numMark;
+        let numEmpty = 3 - markCount;
         if (mark == '') numEmpty = 3;
 
         let matchingLines = board.lines.filter(line => {
@@ -35,12 +36,12 @@ const botFactory = (mode) => {
                 if (board.cells[cell] == '') countEmpty++
             });
 
-            if (countMark == numMark) {
+            if (countMark == markCount) {
                 if (countEmpty == numEmpty) return true
             }
 
         });
-        
+
         return filterDublicates(
             concatArrays(matchingLines).filter(
                 cell => board.cells[cell] == ''
@@ -51,16 +52,16 @@ const botFactory = (mode) => {
     const marksCell = (lines) => {
 
         let continues = false;
-        if (mark == 'O') if (lines.length) continues = true;
+        if (this.turn == true) if (lines.length) continues = true;
 
         if (continues) lines.forEach(cell => {
             if (continues) if (board.cells[cell] == '') {
                 board.cells[cell] = 'O';
                 const targetCell = document.querySelector(`[cell = '${cell}']`);
                 targetCell.textContent = 'O';
-                playRound();
-                mark = 'X';
-                continues = false
+                continues = false;
+                this.turn = false;
+                player.plays()
             }
         })
 
@@ -70,63 +71,63 @@ const botFactory = (mode) => {
 
     const plays = {
         impossible: () => {
+            this.turn = true;
+            let linesTwoO = getLines(2, 'O');
+            let linesTwoX = getLines(2, 'X');
+            let linesOneX = getLines(1, 'X');
+            let linesOneO = getLines(1, 'O');
 
-            let linesWithTwoO = getLinesWith(2, 'O');
-            let linesWithTwoX = getLinesWith(2, 'X');
-            let linesWithOneX = getLinesWith(1, 'X');
-            let linesWithOneO = getLinesWith(1, 'O');
-
-            marksCell(linesWithTwoO);
-            marksCell(linesWithTwoX);
+            marksCell(linesTwoO);
+            marksCell(linesTwoX);
 
             if (board.cells[5] == '') {
 
-                if (linesWithOneX.length == 6) marksCell([5])
-                else if (linesWithOneX.length == 4) marksCell([5])
-                else if (linesWithOneO.length) marksCell(linesWithOneX.filter(cell => [1, 3, 7, 9].includes(cell)))
-                else marksCell(linesWithOneX.filter(cell => [2, 4, 6, 8].includes(cell)))
+                if (linesOneX.length == 6) marksCell([5])
+                else if (linesOneX.length == 4) marksCell([5])
+                else if (linesOneO.length) marksCell(linesOneX.filter(cell => [1, 3, 7, 9].includes(cell)))
+                else marksCell(linesOneX.filter(cell => [2, 4, 6, 8].includes(cell)))
 
-            } else if (board.cells[5] == 'X') marksCell(linesWithOneX.filter(cell => [1, 3, 7, 9].includes(cell)))
-            else if (linesWithOneX.length == 6) marksCell(linesWithOneX.filter(cell => [2, 4, 6, 8].includes(cell)))
-            else marksCell(linesWithOneX.filter(cell => [1, 3, 7, 9].includes(cell)));
-            marksCell(linesWithOneX)
+            } else if (board.cells[5] == 'X') marksCell(linesOneX.filter(cell => [1, 3, 7, 9].includes(cell)))
+            else if (linesOneX.length == 6) marksCell(linesOneX.filter(cell => [2, 4, 6, 8].includes(cell)))
+            else marksCell(linesOneX.filter(cell => [1, 3, 7, 9].includes(cell)));
+            marksCell(linesOneX)
         },
 
         hard: () => {
+            this.turn = true;
+            let linesOneO = getLines(1, 'O');
+            let linesTwoO = getLines(2, 'O');
+            let linesTwoX = getLines(2, 'X');
+            let linesOneX = getLines(1, 'X');
 
-            let linesWithOneO = getLinesWith(1, 'O');
-            let linesWithTwoO = getLinesWith(2, 'O');
-            let linesWithTwoX = getLinesWith(2, 'X');
-            let linesWithOneX = getLinesWith(1, 'X');
-
-            marksCell(linesWithTwoO);
-            marksCell(linesWithTwoX);
+            marksCell(linesTwoO);
+            marksCell(linesTwoX);
             if (board.cells[5] == '') {
-                if (linesWithOneX.length == 6) marksCell([5])
-                else marksCell(linesWithOneX.filter(cell => [2, 4, 6, 8].includes(cell)))
-            } else if (board.cells[5] == 'X') marksCell(linesWithOneX.filter(cell => [1, 3, 7, 9].includes(cell)))
-            else marksCell(linesWithOneX.filter(cell => [2, 4, 6, 8].includes(cell)));
-            marksCell(linesWithOneX);
-            marksCell(linesWithOneO)
+                if (linesOneX.length == 6) marksCell([5])
+                else marksCell(linesOneX.filter(cell => [2, 4, 6, 8].includes(cell)))
+            } else if (board.cells[5] == 'X') marksCell(linesOneX.filter(cell => [1, 3, 7, 9].includes(cell)))
+            else marksCell(linesOneX.filter(cell => [2, 4, 6, 8].includes(cell)));
+            marksCell(linesOneX);
+            marksCell(linesOneO)
         },
 
         easy: () => {
+            this.turn = true;
+            let cleanLine = getLines(3, '');
+            let hasEmpty = getLines(1, '');
+            let linesOneX = getLines(1, 'X');
+            let linesTwoO = getLines(2, 'O');
+            let linesTwoX = getLines(2, 'X');
 
-            let cleanLine = getLinesWith(3, '');
-            let hasEmpty = getLinesWith(1, '');
-            let linesWithOneX = getLinesWith(1, 'X');
-            let linesWithTwoO = getLinesWith(2, 'O');
-            let linesWithTwoX = getLinesWith(2, 'X');
-
-            marksCell(linesWithTwoO);
-            marksCell(linesWithTwoX);
-            marksCell(linesWithOneX);
+            marksCell(linesTwoO);
+            marksCell(linesTwoX);
+            marksCell(linesOneX);
             marksCell(cleanLine);
             marksCell(hasEmpty)
 
         }
     };
 
-    return { plays:plays[mode], score }
+    return { plays: plays[mode], score, turn }
 
 };
--- main.js
+++ main.js
@@ -1,25 +1,86 @@
-const gridCells = document.querySelectorAll('[cell]');
+const cells = document.querySelectorAll('[cell]');
 const message = document.querySelector('.message');
 const modeBtns = document.querySelectorAll('.modes > .button');
 const crown = document.createElement('img');
 crown.src = './crown.svg';
 
+modeBtns.forEach(button => button.addEventListener('click', (e) => {
+    document.querySelector('.selected').classList.toggle('selected');
+    e.target.classList.add('selected');
+    game.mode = e.target.textContent;
+    newGame(game.mode)
+}));
+
+cells.forEach(cell => cell.addEventListener('click', (e) => {
+
+    const cellMark = e.target.textContent;
+    const cellNumber = e.target.getAttribute('cell');
+
+    if (!player.turn) return
+    if (game.over() || cellMark == 'O') return
+    if (cellMark != '') return
+    e.target.textContent = 'X';
+    board.cells[cellNumber] = 'X';
+    player.plays();
+    setTimeout(playRound, 500);
+
+}));
+
+message.addEventListener('click', () => {
+    if (message.textContent == 'Play again?') {
+        newGame(game.mode);
+        refreshScore({ match: false, mark: 'X' })
+    }
+});
+
+class Display {
+
+    constructor() {
+        this.cells = document.querySelectorAll('[cell]');
+        this.message = document.querySelector('.message');
+        this.crown = crown
+    }
+
+    score() {
+        message.textContent = `round ${round.count}`;
+        document.querySelector('.player.one .score').textContent = `Player One ${player.score}`;
+        document.querySelector('.player.two .score').textContent = `Player Two ${bot.score}`
+    }
+
+    winner() {
+        const elem = document.querySelector(`.player.${!player.turn?'one':'two'} > .crown`)
+        elem.appendChild(crown);
+        elem.firstChild.classList.add('animate');
+        message.textContent = 'Play again?'
+    }
+
+    removeCrown() {
+        if (document.querySelector('.crown img')) document.querySelector('.crown img').remove()
+    }
+
+}
+
 class Game {
     constructor() {
-        this.roundCount = 1;
+        this.mode = 'hard';
     }
 
-    newRound() {
-        this.refreshDisplay();
-        gridCells.forEach(cell => cell.textContent = '');
-        board = new Board;
-        mark = 'X';
-        this.resetPlayingClass()
+    over() {
+        return (player.score == 3 || bot.score == 3)
+    }
+};
+
+class Round {
+    constructor() {
+        this.count = 1
     }
 
-    roundState(mark) {
+    state() {
+        let mark = undefined
+        if (!player.turn) mark = 'X'
+        else mark = 'O';
 
-        let match = false;
+        let over = false;
         let winningLine = [];
 
         board.lines.forEach(line => {
@@ -29,44 +90,20 @@ class Game {
                 if (board.cells[cell] == mark) count++
             });
 
-            if (count == 3) match = true, winningLine = line
+            if (count == 3) over = true, winningLine = line
             else count = 0
         });
 
-        return { match, mark, winningLine }
+        return { over, mark, winningLine }
     }
 
-    refreshDisplay() {
-        message.textContent = `round ${this.roundCount}`;
-        document.querySelector('.player.one .score').textContent = `Player One ${player.score}`;
-        document.querySelector('.player.two .score').textContent = `Player Two ${bot.score}`
+    rebuild() {
+        display.score();
+        cells.forEach(cell => cell.textContent = '');
+        player.turn = true;
+        board = new Board;
     }
 
-    displayWinner(mark) {
-        if (mark == 'X') {
-            document.querySelector('.player.one > .crown').appendChild(crown);
-            document.querySelector('.player.one > .crown').firstChild.classList.add('animate')
-        } else {
-            document.querySelector('.player.two > .crown').appendChild(crown);
-            document.querySelector('.player.two > .crown').firstChild.classList.add('animate')
-        };
-        message.textContent = 'Play again?'
-    }
-
-    removeCrown() {
-        if (document.querySelector('.crown img')) document.querySelector('.crown img').remove()
-    }
-
-    resetPlayingClass() {
-        document.querySelector('.player.one .score').classList.remove('playing');
-        document.querySelector('.player.two .score').classList.remove('playing');
-        document.querySelector('.player.one .score').classList.add('playing')
-    }
-
-    shiftPlayingClass() {
-        document.querySelector('.player.one .score').classList.toggle('playing')
-        document.querySelector('.player.two .score').classList.toggle('playing')
-    }
 };
 
 class Player {
@@ -82,14 +119,9 @@ class Board {
     constructor() {
         this.cells = this.createCells(9, '');
         this.lines = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [1, 4, 7],
-            [2, 5, 8],
-            [3, 6, 9],
-            [1, 5, 9],
-            [3, 5, 7]
+            [1, 2, 3], [4, 5, 6], [7, 8, 9],
+            [1, 4, 7], [2, 5, 8], [3, 6, 9],
+            [1, 5, 9], [3, 5, 7]
         ]
     }
 
@@ -101,90 +133,55 @@ class Board {
         return object
     }
 
-    hasEmptyCell() {
-
+    filled() {
         for (let key in this.cells) {
-            if (this.cells[key] == '') return true
+            if (this.cells[key] == '') return false
         };
-
-        return false
+        return true
     }
 };
 
-let mark = 'X';
-let mode = 'hard';
 let game = new Game;
+let round = new Round;
 let board = new Board;
 let player = new Player;
-let bot = botFactory(mode);
+let display = new Display;
+let bot = botFactory(game.mode);
 
-modeBtns.forEach(button => button.addEventListener('click', (e) => {
-    document.querySelector('.selected').classList.toggle('selected');
-    e.target.classList.add('selected');
-    mode = e.target.textContent;
-    newGame(mode)
-}));
-
-gridCells.forEach(cell => cell.addEventListener('click', (e) => {
-    const mark = e.target.textContent;
-    const cellNumber = e.target.getAttribute('cell');
-
-    //if (!player.turn) return
-    //else player.plays();
-
-    if (isGameOver() || mark == 'O') return
-    if (mark == '') {
-        e.target.textContent = 'X';
-        board.cells[cellNumber] = 'X';
-        if (!isGameOver()) playRound();
-    }
-}));
-
-message.addEventListener('click', () => {
-    if (message.textContent == 'Play again?') {
-        newGame();
-        updateState({ match: false, mark: 'X' })
-    }
-});
-///////////////////////////////////////////////////////////////////////////
-
-const updateState = (state) => {
-    if (state.mark == 'X') {
-        if (state.match) player.score++
-    }
-    else {
-        if (state.match) bot.score++
-    };
-    game.refreshDisplay();
-    game.roundCount++;
-};
-
-const isGameOver = () => {
-    return (player.score == 3 || bot.score == 3)
+const refreshScore = () => {
+    if (player.turn) bot.score++
+    else player.score++
+    display.score();
+    round.count++;
 };
 
 const newGame = (mode) => {
     bot = botFactory(mode);
     player = new Player;
     game = new Game;
-    game.removeCrown();
-    game.refreshDisplay();
-    game.newRound()
+    display.removeCrown();
+    display.score();
+    round.rebuild()
 };
 
 const playRound = () => {
 
-    game.shiftPlayingClass();
-    const state = game.roundState(mark);
-
-    if (mark == 'X') mark = 'O', setTimeout(bot.plays, 1000);
-    else mark = 'X';
-
-    if (state.match) {
-        updateState(state);
-        isGameOver() ? game.displayWinner(state.mark) : game.newRound()
+    if (board.filled()) {
+        round.count++;
+        round.rebuild();
+        return
     };
 
-    if (!board.hasEmptyCell()) game.roundCount++, game.newRound();
-
+    if (round.state().over) {
+        refreshScore();
+        if (game.over()) display.winner()
+        else round.rebuild()
+    } else {
+        bot.plays()
+        if (round.state().over) {
+            refreshScore();
+            if (game.over()) display.winner()
+            else round.rebuild()
+        }
+    }
 };
